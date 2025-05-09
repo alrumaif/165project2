6c74b59f90cf90d52accba1c2b998154bb51bd2e
alrumaif <alrumaif@uci.edu>
2025-05-08 23:25:35 -0700

    more edits

--- best_fit.py
+++ best_fit.py
@@ -4,3 +4,6 @@
 
 def best_fit(items: list[float], assignment: list[int], free_space: list[float]):
     pass
+
+def best_fit_decreasing(items: list[float], assignment: list[int], free_space: list[float]):
+    pass
--- first_fit.py
+++ first_fit.py
@@ -4,3 +4,6 @@
 
 def first_fit(items: list[float], assignment: list[int], free_space: list[float]):
     pass
+
+def first_fit_decreasing(items: list[float], assignment: list[int], free_space: list[float]):
+    pass
