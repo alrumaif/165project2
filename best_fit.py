a00eeb591939dfc0a427953b60a17ce1ad46df74
alrumaif <alrumaif@uci.edu>
2025-05-08 23:24:03 -0700

    minor edits

---
+++ best_fit.py
@@ -0,0 +1,6 @@
+# Example file: best_fit.py
+
+# explanations for member functions are provided in requirements.py
+
+def best_fit(items: list[float], assignment: list[int], free_space: list[float]):
+    pass
---
+++ first_fit.py
@@ -0,0 +1,6 @@
+# Example file: first_fit.py
+
+# explanations for member functions are provided in requirements.py
+
+def first_fit(items: list[float], assignment: list[int], free_space: list[float]):
+    pass
