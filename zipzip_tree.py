8e6b5c8a9459e2c306d00455a38cb22100974da5
alrumaif <alrumaif@uci.edu>
2025-05-09 18:34:15 -0700

    z

--- zipzip_tree.py
+++ zipzip_tree.py
@@ -46,51 +46,46 @@ class ZipZipTree:
 	def insert(self, key: KeyType, val: ValType, rank: Rank = None):
 		if rank is None:
 			rank = self.get_random_rank()
-			
 		new_node = Node(key, val, rank)
 		self.tree[key] = new_node
-		
 		cur = self.root
 		parent = None
-		
 		while cur is not None:
 			cur_node = self.tree[cur]
-			if (cur_node.rank.geometric_rank > rank.geometric_rank or
+			if (cur_node.rank.geometric_rank < rank.geometric_rank or
                 (cur_node.rank.geometric_rank == rank.geometric_rank and
-                cur_node.rank.uniform_rank > rank.uniform_rank)):
+                cur_node.rank.uniform_rank < rank.uniform_rank)):
 				parent = cur
-				if key < self.tree[cur].key:
+				if key < cur_node.key:
 					cur = cur_node.left
 				else:
 					cur = cur_node.right
 			else:
 				break
-			
 		if cur is not None:
 			if key < self.tree[cur].key:
 				new_node.right = cur
 			else:
 				new_node.left = cur
-				
 		if parent is None:
 			self.root = key
 		else:
-			# parent_node = self.tree[parent]
-			if key < self.tree[parent].key:
-				self.tree[parent].left = key
+			parent_node = self.tree[parent]
+			if key < parent_node.key:
+				parent_node.left = key
 			else:
-				self.tree[parent].right = key
-				
+				parent_node.right = key
 		self.size += 1
+
 	
     
 	def remove(self, key: KeyType):
 		if key not in self.tree:
 			return
 		cur = self.root
-		prev = None
+		parent = None
 		while cur != key:
-			prev = cur
+			parent = cur
 			if key < self.tree[cur].key:
 				cur = self.tree[cur].left
 			else:
@@ -108,23 +103,24 @@ class ZipZipTree:
 			replacement = right
 		if cur == self.root:
 			self.root = replacement
-		elif key < self.tree[prev].key:
-			self.tree[prev].left = replacement
+		elif key < self.tree[parent].key:
+			self.tree[parent].left = replacement
 		else:
-			self.tree[prev].right = replacement
+			self.tree[parent].right = replacement
 		while left is not None and right is not None:
 			if self.tree[left].rank.geometric_rank <= self.tree[right].rank.geometric_rank:
 				while left is not None and self.tree[left].rank.geometric_rank <= self.tree[right].rank.geometric_rank:
-					prev = left
+					parent = left
 					left = self.tree[left].right
-				self.tree[prev].right = right
+				self.tree[parent].right = right
 			else:
 				while right is not None and self.tree[right].rank.geometric_rank < self.tree[left].rank.geometric_rank:
-					prev = right
+					parent = right
 					right = self.tree[right].left
-				self.tree[prev].left = left
+				self.tree[parent].left = left
 		del self.tree[key]
 		self.size -= 1
+
 
 	
 
